755ed7c9638c32911ef837b25a778eff61d78f3f
friedasong <tsunching1l@gmail.com>
2025-02-02 15:43:56 +0800

    Updated quartz.layout.ts for Giscus integration

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+  Component.Comments({
+    provider: 'giscus',
+    options: {
+      // from data-repo
+      repo: 'friedasong/Thoughts-of-Frieda',
+      // from data-repo-id
+      repoId: 'R_kgDONzBIQA',
+      // from data-category
+      category: 'Announcements',
+      // from data-category-id
+      categoryId: 'DIC_kwDONzBIQM4CmjYP',
+    }
+  }),
+],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
